7db15a598c9ba290bebcea14a1770e37eded40c3
MostafaBastawyy <135256456+MostafaBastawyy@users.noreply.github.com>
2025-06-24 15:13:10 +0300

    remove english from languages list

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -241,7 +241,6 @@ class AppConstants {
   static const String longitude = 'longitude';
   static const String cookiesManagement = 'cookies_management';
 
-
   ///Refer & Earn work flow list..
   static final dataList = [
     'invite_your_friends_and_business'.tr,
@@ -250,7 +249,7 @@ class AppConstants {
   ];
 
   /// Delivery Tips
-  static List<String> tips = ['0' ,'15', '10', '20', '40', 'custom'];
+  static List<String> tips = ['0', '15', '10', '20', '40', 'custom'];
   static List<String> deliveryInstructionList = [
     'deliver_to_front_door',
     'deliver_the_reception_desk',
@@ -285,45 +284,22 @@ class AppConstants {
   static List<LanguageModel> languages = [
     LanguageModel(imageUrl: Images.arabic, languageName: 'عربى', countryCode: 'SA', languageCode: 'ar'),
 
-    LanguageModel(imageUrl: Images.english, languageName: 'English', countryCode: 'US', languageCode: 'en'),
-   // LanguageModel(imageUrl: Images.spanish, languageName: 'Spanish', countryCode: 'ES', languageCode: 'es'),
-   // LanguageModel(imageUrl: Images.bengali, languageName: 'Bengali', countryCode: 'BN', languageCode: 'bn'),
+    //LanguageModel(imageUrl: Images.english, languageName: 'English', countryCode: 'US', languageCode: 'en'),
+    // LanguageModel(imageUrl: Images.spanish, languageName: 'Spanish', countryCode: 'ES', languageCode: 'es'),
+    // LanguageModel(imageUrl: Images.bengali, languageName: 'Bengali', countryCode: 'BN', languageCode: 'bn'),
   ];
 
-  static List<String> joinDropdown = [
-    'join_us',
-    'become_a_seller',
-    'become_a_delivery_man'
-  ];
+  static List<String> joinDropdown = ['join_us', 'become_a_seller', 'become_a_delivery_man'];
 
   static final List<Map<String, String>> walletTransactionSortingList = [
-    {
-      'title' : 'all_transactions',
-      'value' : 'all'
-    },
-    {
-      'title' : 'order_transactions',
-      'value' : 'order'
-    },
-    {
-      'title' : 'converted_from_loyalty_point',
-      'value' : 'loyalty_point'
-    },
-    {
-      'title' : 'added_via_payment_method',
-      'value' : 'add_fund'
-    },
-    {
-      'title' : 'earned_by_referral',
-      'value' : 'referrer'
-    },
-    {
-      'title' : 'cash_back_transactions',
-      'value' : 'CashBack'
-    },
+    {'title': 'all_transactions', 'value': 'all'},
+    {'title': 'order_transactions', 'value': 'order'},
+    {'title': 'converted_from_loyalty_point', 'value': 'loyalty_point'},
+    {'title': 'added_via_payment_method', 'value': 'add_fund'},
+    {'title': 'earned_by_referral', 'value': 'referrer'},
+    {'title': 'cash_back_transactions', 'value': 'CashBack'},
   ];
 
   //taxi seats..
   static List<String> seats = ['1-4', '5-8', '9-13', '14+'];
-
 }
